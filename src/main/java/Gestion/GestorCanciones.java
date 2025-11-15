5c3fd8b1e7b3c21dc16606b95edd8aec5397b774
vindasalvarez1-hub <vindasalvarez1@gmail.com>
2025-11-15 15:14:28 -0600

    Agregando clases Gestor Canciones y Usuarios

---
+++ src/main/java/Gestion/GestorCanciones.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Gestion;
+
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.Gson;
+import java.io.*;
+import java.util.*;
+import Modelos.*;
+
+/**
+ *
+ * @author PC
+ */
+public class GestorCanciones {
+    private static final String FILE_PATH = "canciones.json";
+    private static final Gson gson = new Gson();
+}
