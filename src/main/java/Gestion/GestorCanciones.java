1373918e46a1977ae3af118ea76afab2ff240792
vindasalvarez1-hub <vindasalvarez1@gmail.com>
2025-11-15 15:30:41 -0600

    Agregando cargar y guardar canciones en el gestor

--- src/main/java/Gestion/GestorCanciones.java
+++ src/main/java/Gestion/GestorCanciones.java
@@ -17,4 +17,21 @@ import Modelos.*;
 public class GestorCanciones {
     private static final String FILE_PATH = "canciones.json";
     private static final Gson gson = new Gson();
+
+    public static List<Cancion> cargarCanciones() {
+        try (Reader reader = new FileReader(FILE_PATH)) {
+            return gson.fromJson(reader, new TypeToken<List<Cancion>>(){}.getType());
+        } catch (IOException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    public static void guardarCanciones(List<Cancion> canciones) {
+        try (Writer writer = new FileWriter(FILE_PATH)) {
+            gson.toJson(canciones, writer);
+        } catch (IOException e) {
+            System.out.println("Error guardando canciones: " + e.getMessage());
+        }
+    }
+
 }
